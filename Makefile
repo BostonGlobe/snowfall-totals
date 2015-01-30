2a2f0ddbc30e3fe2c7391644720ac3a3440e553c
Gabriel Florit <gabrielflorit@gmail.com>
2015-01-30 12:18:12 -0500

    check in shpaefiles

--- Makefile
+++ Makefile
@@ -12,3 +12,16 @@ R_deploy:
 css:
 
 	cp node_modules/leaflet/dist/leaflet.css graphics/map/css/_leaflet.scss
+
+download:
+	cd data; \
+		rm -rf input; \
+		mkdir input; \
+		cd input; \
+			curl http://wsgw.mass.gov/data/gispub/shape/ne/cttowns.zip > cttowns.zip; unzip cttowns.zip; \
+			curl http://wsgw.mass.gov/data/gispub/shape/state/townssurvey_shp.zip > matowns.zip; unzip matowns.zip; \
+			curl http://wsgw.mass.gov/data/gispub/shape/ne/metowns.zip > metowns.zip; unzip metowns.zip; \
+			curl http://wsgw.mass.gov/data/gispub/shape/ne/nhtowns.zip > nhtowns.zip; unzip nhtowns.zip; \
+			curl http://wsgw.mass.gov/data/gispub/shape/ne/nytowns.zip > nytowns.zip; unzip nytowns.zip; \
+			curl http://wsgw.mass.gov/data/gispub/shape/ne/ritowns.zip > ritowns.zip; unzip ritowns.zip; \
+			curl http://wsgw.mass.gov/data/gispub/shape/ne/vttowns.zip > vttowns.zip; unzip vttowns.zip;
--- graphics/map/js/main.js
+++ graphics/map/js/main.js
@@ -11,10 +11,12 @@ var map = L.map($map.get(0), {
 	attributionControl: false,
 	scrollWheelZoom: false
 }).setView([42.25841962, -71.81532837], 6);
+// }).setView([0, 0], 2);
 
 // Add the MapBox baselayer to our map.
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/gabriel-florit.baselayer_land/{z}/{x}/{y}.png', {
-	minZoom: 5,
+// L.tileLayer('http://{s}.tiles.mapbox.com/v3/gabriel-florit.207de5da/{z}/{x}/{y}.png', {
+	minZoom: 2,
 	maxZoom: 10
 }).addTo(map);
 
@@ -24,7 +26,8 @@ var southWest = new L.LatLng(30.8, -85.7),
 	bounds = new L.LatLngBounds(southWest, northEast);
 
 // Add the snowfall image to the map.
-var imageLayer = L.imageOverlay('http://amzncache.boston.com/partners/maps/snowfall.png', bounds).addTo(map);
+// var imageLayer = L.imageOverlay('http://amzncache.boston.com/partners/maps/snowfall.png', bounds).addTo(map);
+var imageLayer = L.imageOverlay('js/snowfall.png', bounds).addTo(map);
 
 // Create a Leaflet control for the legend.
 var MyControl = L.Control.extend({
@@ -57,3 +60,70 @@ var hourDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), dat
 
 // Populate the 'updated' element.
 $('.updated-timestamp').html('Updated ' + [APDateTime.time(hourDate), APDateTime.date(hourDate)].join(', '));
+
+
+
+
+
+
+var points = require('./points.json');
+
+var ICON_WIDTH = 38 + 10;
+var ICON_HEIGHT = 18 + 10;
+
+var markers = [];
+
+function intersectRect(r1, r2) {
+	return !(r2.left > r1.right || 
+	r2.right < r1.left || 
+	r2.top > r1.bottom ||
+	r2.bottom < r1.top);
+}
+
+// for each point,
+// find the absolute pixel coordinates for the given zoom level
+// assuming we're center aligning the point,
+// find the point's bounding box in pixel coordinates
+points.forEach(function(point, index) {
+
+	var pointCoords = map.project([point.latitude, point.longitude]);
+
+	var pointBBox = {
+		left: pointCoords.x - ICON_WIDTH/2,
+		right: pointCoords.x + ICON_WIDTH/2,
+		bottom: pointCoords.y + ICON_HEIGHT/2,
+		top: pointCoords.y - ICON_HEIGHT/2
+	};
+
+	// make sure this bbox doesn't overlap with any existing markers
+	var overlaps = _.some(markers, function(marker) {
+
+		var markerCoords = map.project(marker._latlng);
+
+		var markerBBox = {
+			left: markerCoords.x - ICON_WIDTH/2,
+			right: markerCoords.x + ICON_WIDTH/2,
+			bottom: markerCoords.y + ICON_HEIGHT/2,
+			top: markerCoords.y - ICON_HEIGHT/2
+		};
+
+		return intersectRect(pointBBox, markerBBox);
+	});
+
+	// if it doesn't overlap, add to markers layer
+	if (!overlaps) {
+
+		var icon = L.divIcon({
+			html: '<span>' + point.snowfall + '”</span>',
+			className: 'snowfall',
+			iconSize: 100
+		});
+
+		var marker = L.marker([point.latitude, point.longitude], {icon: icon});
+		markers.push(marker);
+
+	}
+
+});
+
+L.layerGroup(markers).addTo(map);
